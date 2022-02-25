9b62be76d96af085cd65d9c724097af2360ea6f5
Luibov Kapitalskaya <lkapitalskaya@bhm-3123.lan>
2022-02-25 15:49:04 -0600

    js challanges solved

--- Challanges/script.js
+++ Challanges/script.js
@@ -119,3 +119,388 @@ console.log(myFunction5({ x: 'a', y: 'b', z: 'c' }, 'a'));
 console.log(myFunction5({ x: 'a', y: 'b', z: 'c' }, 'z'));
 // Expected
 // true
+
+// *
+// *
+// *
+
+// Challenge #6
+
+// Write a function that takes an array (a) and a number (b) as arguments
+// Sum up all array elements with a value greater than b
+// Return the sum
+function myFunction6(a, b) {
+  const sum = a
+    .filter((num) => num > b)
+    .reduce(
+      (previousValue, currentValue) =>
+        previousValue + currentValue
+    );
+  return sum;
+}
+
+console.log(myFunction6([1, 2, 3, 4, 5, 6, 7], 2));
+// Expected
+// 25
+
+console.log(myFunction6([-10, -11, -3, 1, -4], -3));
+// Expected
+// 1
+
+console.log(myFunction6([78, 99, 100, 101, 401], 99));
+// Expected
+// 602
+
+// Challenge #7
+
+// Write a function that takes a value as argument
+// Return the type of the value
+function myFunction7(a) {
+  return typeof a;
+}
+
+console.log(myFunction7(1));
+// Expected
+// 'number'
+
+console.log(myFunction7(false));
+// Expected
+// 'boolean'
+
+console.log(myFunction7({}));
+// Expected
+// 'object'
+myFunction7(null);
+console.log(myFunction7(null));
+// Expected
+// 'object'
+
+console.log(myFunction7('string'));
+// Expected
+// 'string'
+
+console.log(myFunction7(['array']));
+// Expected
+// 'object'
+
+// *
+// *
+// *
+
+// Challenge #8
+
+// Write a function that a string (a) as argument
+// Create an object that has a property with key 'key' and a value of a
+// Return the object
+function myFunction8(a) {
+  const o = new Object();
+  o.key = a;
+  return o;
+}
+
+console.log(myFunction8('a'));
+// Expected
+// {key:'a'}
+
+console.log(myFunction8('z'));
+// Expected
+// {key:'z'}
+
+console.log(myFunction8('b'));
+// Expected
+// {key:'b'}
+
+// *
+// *
+// *
+
+//Challenge #9
+
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
+function myFunction9(a) {
+  return a.length;
+}
+
+console.log(myFunction9([1, 2, 2, 4]));
+// Expected
+// 4
+
+console.log(myFunction9([9, 9, 9]));
+// Expected
+// 3
+
+console.log(myFunction9([4, 3, 2, 1, 0]));
+// Expected
+// 5
+
+// *
+// *
+// *
+
+//Challange #10
+
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+function myFunction10(a, b) {
+  return a + b;
+}
+
+console.log(myFunction10(1, 2));
+// Expected
+// 3
+
+console.log(myFunction10(1, 10));
+// Expected
+// 11
+
+console.log(myFunction10(99, 1));
+// Expected
+// 100
+
+// *
+// *
+// *
+
+//Challenge #11
+
+// Write a function that takes 6 values (a,b,c,d,e,f) as arguments
+// Sum a and b
+// Then substract by c
+// Then multiply by d and divide by e
+// Finally raise to the power of f and return the result
+// Tipp: mind the order
+function myFunction11(a, b, c, d, e, f) {
+  return;
+}
+
+// Write a function that takes 6 values (a,b,c,d,e,f) as arguments
+// Sum a and b
+// Then substract by c
+// Then multiply by d and divide by e
+// Finally raise to the power of f and return the result
+// Tipp: mind the order
+function myFunction11(a, b, c, d, e, f) {
+  return (((a + b - c) * d) / e) ** f;
+}
+
+console.log(myFunction11(6, 5, 4, 3, 2, 1));
+// Expected
+// 10.5
+
+console.log(myFunction11(6, 2, 1, 4, 2, 3));
+// Expected
+// 2744
+
+console.log(myFunction11(2, 3, 6, 4, 2, 3));
+// Expected
+// -8
+
+// *
+// *
+// *
+
+//Challenge #12
+
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+function myFunction12(a) {
+  return a.slice(0, a.length / 2);
+}
+
+console.log(myFunction12('abcdefgh'));
+// Expected
+// 'abcd'
+console.log(myFunction12('1234'));
+// Expected
+// '12'
+console.log(myFunction12('gedcba'));
+// Expected
+// 'ged'
+
+//Challenge # 13
+
+// *
+// *
+// *
+
+// Write a function that takes an object with two properties and a string as arguments
+// It should return the value of the property with key equal to the value of the string
+function myFunction13(obj, key) {
+  return obj[key];
+}
+
+console.log(
+  myFunction13(
+    { continent: 'Asia', country: 'Japan' },
+    'continent'
+  )
+);
+// Expected
+// 'Asia'
+console.log(
+  myFunction13(
+    { country: 'Sweden', continent: 'Europe' },
+    'country'
+  )
+);
+
+// Expected
+// 'Sweden'
+
+// *
+// *
+// *
+
+// Challange #14
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+function myFunction14(a, b) {
+  if (a === b) {
+    return true;
+  }
+  return false;
+  // return a===b
+}
+
+console.log(myFunction14(2, 3));
+// Expected
+// false
+console.log(myFunction14(3, 3));
+
+// Expected
+// true
+console.log(myFunction14(1, '1'));
+
+// Expected
+// false
+console.log(myFunction14('10', '10'));
+
+// Expected
+// true
+
+// *
+// *
+// *
+
+// Challange #15
+
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+function myFunction15(a) {
+  return a.slice(0, 3);
+}
+
+console.log(myFunction15('abcdefg'));
+// Expected;
+// ('abc');
+
+console.log(myFunction15('1234'));
+// Expected;
+// ('123');
+
+console.log(myFunction15('fgedcba'));
+// Expected;
+// ('fge');
+
+// *
+// *
+// *
+
+// Challange #16
+
+// Write a function that takes an array (a) as argument
+// Extract the first 3 elements of a
+// Return the resulting array
+function myFunction16(a) {
+  return a.splice(0, 3);
+}
+
+console.log(myFunction16([1, 2, 3, 4]));
+// Expected
+// [1,2,3]
+
+console.log(myFunction16([5, 4, 3, 2, 1, 0]));
+// Expected
+// [5,4,3]
+console.log(myFunction16([99, 1, 1]));
+
+// Expected
+// [99,1,1]
+
+// *
+// *
+// *
+
+// Challange #17
+
+// Write a function that takes an array (a) and a value (n) as argument
+// Return the nth element of 'a'
+function myFunction17(a, n) {
+  return a[n - 1];
+}
+
+console.log(myFunction17([1, 2, 3, 4, 5], 3));
+// Expected
+// 3
+
+console.log(myFunction17([10, 9, 8, 7, 6], 5));
+// Expected
+// 6
+
+console.log(myFunction17([7, 2, 1, 6, 3], 1));
+// Expected
+// 7
+
+//Challenge #18
+// Write a function that takes an array of numbers as argument
+// Return the number of negative values in the array
+function myFunction18(a) {
+  const positive = [];
+  a.forEach((el, i) => {
+    el < 0 && positive.push(el);
+  });
+  return positive.length;
+  // return a.filter((el) => el < 0).length;
+}
+
+console.log(myFunction18([1, -2, 2, -4]));
+// Expected
+// 2
+
+console.log(myFunction18([0, 9, 1]));
+// Expected
+// 0
+
+console.log(myFunction18([4, -3, 2, 1, 0]));
+// Expected
+// 1
+
+// *
+// *
+// *
+
+// Challange #18
+
+// Write a function that takes a string (a) as argument
+// Remove the last 3 characters of a
+// Return the result
+function myFunction(a) {
+  return a.substr(0, a.length - 3);
+}
+
+console.log(myFunction('abcdefg'));
+// Expected
+// 'abcd'
+
+console.log(myFunction('1234'));
+// Expected
+// '1'
+
+console.log(myFunction('fgedcba'));
+
+// Expected
+// 'fged'
